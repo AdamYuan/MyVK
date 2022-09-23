1b5548ae676e571f7bc8762bb3b60ce91970e2ba
AdamYuan <y13916619121@126.com>
2022-09-23 12:36:59 +0800

    Fetch vulkan11/12/13 properties and features

--- include/myvk/PhysicalDevice.hpp
+++ include/myvk/PhysicalDevice.hpp
@@ -16,10 +16,19 @@ private:
 
 	VkPhysicalDevice m_physical_device{VK_NULL_HANDLE};
 
-	VkPhysicalDeviceProperties m_properties;
 	VkPhysicalDeviceMemoryProperties m_memory_properties;
 	VkPhysicalDeviceFeatures m_features;
-	VkPhysicalDeviceFeatures2 m_features2;
+	VkPhysicalDeviceVulkan11Features m_vk11_features{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_FEATURES,
+	                                                 &m_vk12_features};
+	VkPhysicalDeviceVulkan12Features m_vk12_features{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES,
+	                                                 &m_vk13_features};
+	VkPhysicalDeviceVulkan13Features m_vk13_features{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES};
+	VkPhysicalDeviceProperties m_properties;
+	VkPhysicalDeviceVulkan11Properties m_vk11_properties{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_PROPERTIES,
+	                                                     &m_vk12_properties};
+	VkPhysicalDeviceVulkan12Properties m_vk12_properties{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_PROPERTIES,
+	                                                     &m_vk13_properties};
+	VkPhysicalDeviceVulkan13Properties m_vk13_properties{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_PROPERTIES};
 	std::vector<VkQueueFamilyProperties> m_queue_family_properties;
 
 	void initialize(const Ptr<Instance> &instance, VkPhysicalDevice physical_device);
@@ -29,9 +38,15 @@ public:
 
 	const Ptr<Instance> &GetInstancePtr() const { return m_instance_ptr; }
 	VkPhysicalDevice GetHandle() const { return m_physical_device; }
-	const VkPhysicalDeviceProperties &GetProperties() const { return m_properties; }
 	const VkPhysicalDeviceMemoryProperties &GetMemoryProperties() const { return m_memory_properties; }
+	const VkPhysicalDeviceProperties &GetProperties() const { return m_properties; }
+	const VkPhysicalDeviceVulkan11Properties &GetVk11Properties() const { return m_vk11_properties; }
+	const VkPhysicalDeviceVulkan12Properties &GetVk12Properties() const { return m_vk12_properties; }
+	const VkPhysicalDeviceVulkan13Properties &GetVk13Properties() const { return m_vk13_properties; }
 	const VkPhysicalDeviceFeatures &GetFeatures() const { return m_features; }
+	const VkPhysicalDeviceVulkan11Features &GetVk11Features() const { return m_vk11_features; }
+	const VkPhysicalDeviceVulkan12Features &GetVk12Features() const { return m_vk12_features; }
+	const VkPhysicalDeviceVulkan13Features &GetVk13Features() const { return m_vk13_features; }
 	const std::vector<VkQueueFamilyProperties> &GetQueueFamilyProperties() const { return m_queue_family_properties; }
 	bool GetSurfaceSupport(uint32_t queue_family_index, const Ptr<Surface> &surface) const;
 };
--- src/PhysicalDevice.cpp
+++ src/PhysicalDevice.cpp
@@ -4,9 +4,17 @@ namespace myvk {
 void PhysicalDevice::initialize(const Ptr<Instance> &instance, VkPhysicalDevice physical_device) {
 	m_instance_ptr = instance;
 	m_physical_device = physical_device;
-	vkGetPhysicalDeviceProperties(physical_device, &m_properties);
 	vkGetPhysicalDeviceMemoryProperties(physical_device, &m_memory_properties);
-	vkGetPhysicalDeviceFeatures(physical_device, &m_features);
+	{
+		VkPhysicalDeviceFeatures2 features2{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2, &m_vk11_features};
+		vkGetPhysicalDeviceFeatures2(physical_device, &features2);
+		m_features = features2.features;
+	}
+	{
+		VkPhysicalDeviceProperties2 properties2{VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2, &m_vk11_properties};
+		vkGetPhysicalDeviceProperties2(physical_device, &properties2);
+		m_properties = properties2.properties;
+	}
 	{
 		uint32_t count;
 		vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &count, nullptr);
