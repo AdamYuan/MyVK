af17722aba0787e1d71b1bc66932d675723f5585
AdamYuan <y13916619121@126.com>
2025-03-25 22:03:39 +0800

    Make functions in SyncHelper.hpp constexpr; Add functions for RenderPass synchronization

--- include/myvk/SyncHelper.hpp
+++ include/myvk/SyncHelper.hpp
@@ -6,7 +6,25 @@
 
 namespace myvk {
 
-inline VkAccessFlags2 GetWriteAccessMask2(VkAccessFlags2 access_mask) {
+// TODO: This function should not be placed in SyncHelper.hpp
+inline constexpr VkImageAspectFlags GetFormatImageAspects(VkFormat format) {
+	switch (format) {
+	case VK_FORMAT_D32_SFLOAT:
+	case VK_FORMAT_D16_UNORM:
+	case VK_FORMAT_X8_D24_UNORM_PACK32:
+		return VK_IMAGE_ASPECT_DEPTH_BIT;
+	case VK_FORMAT_D16_UNORM_S8_UINT:
+	case VK_FORMAT_D24_UNORM_S8_UINT:
+	case VK_FORMAT_D32_SFLOAT_S8_UINT:
+		return VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+	case VK_FORMAT_S8_UINT:
+		return VK_IMAGE_ASPECT_STENCIL_BIT;
+	default:
+		return VK_IMAGE_ASPECT_COLOR_BIT;
+	}
+}
+
+inline constexpr VkAccessFlags2 GetWriteAccessMask2(VkAccessFlags2 access_mask) {
 	constexpr VkAccessFlags2 kReadonlyAccessMask =
 	    VK_ACCESS_2_INDIRECT_COMMAND_READ_BIT | VK_ACCESS_2_INDEX_READ_BIT | VK_ACCESS_2_VERTEX_ATTRIBUTE_READ_BIT |
 	    VK_ACCESS_2_UNIFORM_READ_BIT | VK_ACCESS_2_INPUT_ATTACHMENT_READ_BIT | VK_ACCESS_2_SHADER_READ_BIT |
@@ -31,18 +49,20 @@ inline VkAccessFlags2 GetWriteAccessMask2(VkAccessFlags2 access_mask) {
 	return access_mask & (~kReadonlyAccessMask);
 }
 
-inline bool IsAccessMaskReadOnly2(VkAccessFlags2 access_mask) { return GetWriteAccessMask2(access_mask) == 0; }
+inline constexpr bool IsAccessMaskReadOnly2(VkAccessFlags2 access_mask) {
+	return GetWriteAccessMask2(access_mask) == 0;
+}
 
 struct BufferSyncState {
 	VkPipelineStageFlags2 stage_mask{VK_PIPELINE_STAGE_2_NONE};
 	VkAccessFlags2 access_mask{VK_ACCESS_2_NONE};
 
-	BufferSyncState &operator|=(const BufferSyncState &r) {
+	constexpr BufferSyncState &operator|=(const BufferSyncState &r) {
 		stage_mask |= r.stage_mask;
 		access_mask |= r.access_mask;
 		return *this;
 	}
-	BufferSyncState operator|(const BufferSyncState &r) const {
+	constexpr BufferSyncState operator|(const BufferSyncState &r) const {
 		auto l = *this;
 		l |= r;
 		return l;
@@ -54,7 +74,7 @@ struct ImageSyncState {
 	VkAccessFlags2 access_mask{VK_ACCESS_2_NONE};
 	VkImageLayout layout{VK_IMAGE_LAYOUT_UNDEFINED};
 
-	ImageSyncState &operator|=(const ImageSyncState &r) {
+	constexpr ImageSyncState &operator|=(const ImageSyncState &r) {
 		stage_mask |= r.stage_mask;
 		access_mask |= r.access_mask;
 		// assert(layout == VK_IMAGE_LAYOUT_UNDEFINED || r.layout == VK_IMAGE_LAYOUT_UNDEFINED || layout == r.layout);
@@ -62,12 +82,148 @@ struct ImageSyncState {
 			layout = r.layout;
 		return *this;
 	}
-	ImageSyncState operator|(const ImageSyncState &r) const {
+	constexpr ImageSyncState operator|(const ImageSyncState &r) const {
 		auto l = *this;
 		l |= r;
 		return l;
 	}
 };
+
+// Spec 8.5. Render Pass Load Operations
+inline constexpr VkPipelineStageFlags2 GetAttachmentLoadOpStageMask2(VkImageAspectFlags aspects,
+                                                                     VkAttachmentLoadOp load_op) {
+	VkPipelineStageFlags2 ret{};
+	if (load_op != VK_ATTACHMENT_LOAD_OP_NONE_EXT) {
+		if (aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+			ret |= VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
+		if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT))
+			ret |= VK_PIPELINE_STAGE_2_EARLY_FRAGMENT_TESTS_BIT;
+	}
+	return ret;
+}
+inline constexpr VkPipelineStageFlags2 GetAttachmentLoadOpStageMask2(VkImageAspectFlags aspects,
+                                                                     VkAttachmentLoadOp load_op,
+                                                                     VkAttachmentLoadOp stencil_load_op) {
+	return GetAttachmentLoadOpStageMask2(aspects & (VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT), load_op) |
+	       GetAttachmentLoadOpStageMask2(aspects & VK_IMAGE_ASPECT_STENCIL_BIT, stencil_load_op);
+}
+inline constexpr VkAccessFlags2 GetAttachmentLoadOpAccessMask2(VkImageAspectFlags aspects, VkAttachmentLoadOp load_op) {
+	switch (load_op) {
+	case VK_ATTACHMENT_LOAD_OP_LOAD: {
+		VkAccessFlags2 ret{};
+		if (aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+			ret |= VK_ACCESS_2_COLOR_ATTACHMENT_READ_BIT;
+		if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT))
+			ret |= VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_READ_BIT;
+		return ret;
+	}
+	case VK_ATTACHMENT_LOAD_OP_DONT_CARE:
+	case VK_ATTACHMENT_LOAD_OP_CLEAR: {
+		VkAccessFlags2 ret{};
+		if (aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+			ret |= VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT;
+		if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT))
+			ret |= VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+		return ret;
+	}
+	default:
+		return {};
+	}
+}
+inline constexpr VkAccessFlags2 GetAttachmentLoadOpAccessMask2(VkImageAspectFlags aspects, VkAttachmentLoadOp load_op,
+                                                               VkAttachmentLoadOp stencil_load_op) {
+	return GetAttachmentLoadOpAccessMask2(aspects & (VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT), load_op) |
+	       GetAttachmentLoadOpAccessMask2(aspects & VK_IMAGE_ASPECT_STENCIL_BIT, stencil_load_op);
+}
+
+// Spec 8.6. Render Pass Store Operations
+inline constexpr VkPipelineStageFlags2 GetAttachmentStoreOpStageMask2(VkImageAspectFlags aspects,
+                                                                      VkAttachmentStoreOp store_op) {
+	VkPipelineStageFlags2 ret{};
+	if (store_op != VK_ATTACHMENT_STORE_OP_NONE) {
+		if (aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+			ret |= VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT;
+		if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT))
+			ret |= VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT;
+	}
+	return ret;
+}
+inline constexpr VkPipelineStageFlags2 GetAttachmentStoreOpStageMask2(VkImageAspectFlags aspects,
+                                                                      VkAttachmentStoreOp store_op,
+                                                                      VkAttachmentStoreOp stencil_store_op) {
+	return GetAttachmentStoreOpStageMask2(aspects & (VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT), store_op) |
+	       GetAttachmentStoreOpStageMask2(aspects & VK_IMAGE_ASPECT_STENCIL_BIT, stencil_store_op);
+}
+inline constexpr VkAccessFlags2 GetAttachmentStoreOpAccessMask2(VkImageAspectFlags aspects,
+                                                                VkAttachmentStoreOp store_op) {
+	switch (store_op) {
+	case VK_ATTACHMENT_STORE_OP_DONT_CARE:
+	case VK_ATTACHMENT_STORE_OP_STORE: {
+		VkAccessFlags2 ret{};
+		if (aspects & VK_IMAGE_ASPECT_COLOR_BIT)
+			ret |= VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT;
+		if (aspects & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT))
+			ret |= VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+		return ret;
+	}
+	case VK_ATTACHMENT_STORE_OP_NONE:
+	default:
+		return {};
+	}
+}
+inline constexpr VkAccessFlags2 GetAttachmentStoreOpAccessMask2(VkImageAspectFlags aspects,
+                                                                VkAttachmentStoreOp store_op,
+                                                                VkAttachmentStoreOp stencil_store_op) {
+	return GetAttachmentStoreOpAccessMask2(aspects & (VK_IMAGE_ASPECT_COLOR_BIT | VK_IMAGE_ASPECT_DEPTH_BIT),
+	                                       store_op) |
+	       GetAttachmentStoreOpAccessMask2(aspects & VK_IMAGE_ASPECT_STENCIL_BIT, stencil_store_op);
+}
+
+inline constexpr ImageSyncState GetAttachmentLoadOpSync(VkImageAspectFlags aspects, VkAttachmentLoadOp load_op,
+                                                        VkImageLayout initial_layout) {
+	return {.stage_mask = GetAttachmentLoadOpStageMask2(aspects, load_op),
+	        .access_mask = GetAttachmentLoadOpAccessMask2(aspects, load_op),
+	        .layout = initial_layout};
+}
+inline constexpr ImageSyncState GetAttachmentLoadOpSync(VkFormat format, VkAttachmentLoadOp load_op,
+                                                        VkImageLayout initial_layout) {
+	return GetAttachmentLoadOpSync(GetFormatImageAspects(format), load_op, initial_layout);
+}
+inline constexpr ImageSyncState GetAttachmentLoadOpSync(VkImageAspectFlags aspects, VkAttachmentLoadOp load_op,
+                                                        VkAttachmentLoadOp stencil_load_op,
+                                                        VkImageLayout initial_layout) {
+	return {.stage_mask = GetAttachmentLoadOpStageMask2(aspects, load_op, stencil_load_op),
+	        .access_mask = GetAttachmentLoadOpAccessMask2(aspects, load_op, stencil_load_op),
+	        .layout = initial_layout};
+}
+inline constexpr ImageSyncState GetAttachmentLoadOpSync(VkFormat format, VkAttachmentLoadOp load_op,
+                                                        VkAttachmentLoadOp stencil_load_op,
+                                                        VkImageLayout initial_layout) {
+	return GetAttachmentLoadOpSync(GetFormatImageAspects(format), load_op, stencil_load_op, initial_layout);
+}
+
+inline constexpr ImageSyncState GetAttachmentStoreOpSync(VkImageAspectFlags aspects, VkAttachmentStoreOp store_op,
+                                                         VkImageLayout final_layout) {
+	return {.stage_mask = GetAttachmentStoreOpStageMask2(aspects, store_op),
+	        .access_mask = GetAttachmentStoreOpAccessMask2(aspects, store_op),
+	        .layout = final_layout};
+}
+inline constexpr ImageSyncState GetAttachmentStoreOpSync(VkFormat format, VkAttachmentStoreOp store_op,
+                                                         VkImageLayout final_layout) {
+	return GetAttachmentStoreOpSync(GetFormatImageAspects(format), store_op, final_layout);
+}
+inline constexpr ImageSyncState GetAttachmentStoreOpSync(VkImageAspectFlags aspects, VkAttachmentStoreOp store_op,
+                                                         VkAttachmentStoreOp stencil_store_op,
+                                                         VkImageLayout final_layout) {
+	return {.stage_mask = GetAttachmentStoreOpStageMask2(aspects, store_op, stencil_store_op),
+	        .access_mask = GetAttachmentStoreOpAccessMask2(aspects, store_op, stencil_store_op),
+	        .layout = final_layout};
+}
+inline constexpr ImageSyncState GetAttachmentStoreOpSync(VkFormat format, VkAttachmentStoreOp store_op,
+                                                         VkAttachmentStoreOp stencil_store_op,
+                                                         VkImageLayout final_layout) {
+	return GetAttachmentStoreOpSync(GetFormatImageAspects(format), store_op, stencil_store_op, final_layout);
+}
 
 } // namespace myvk
 
